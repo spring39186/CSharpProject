625a2526c8e392c598079d52c8c063a838268942
tim_cheng <tim_cheng@mail.gss.com.tw>
2018-11-27 15:01:48 +0800

    Create Program_Diff.cs
    Test Diff

---
+++ SomeConsoleApplication/Program_Diff.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace SomeConsoleApplication
+{
+    public class ProgramDiffTest
+    {
+        static void Main(string[] args)
+        {
+            var iAmTrue = true;
+			
+			int abc = 8;
+			abc =+ 8 ;
+            if (iAmTrue)
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine("false");
+            }
+
+            Console.ReadKey();
+        }
+
+        public static bool AlwaysReturnsTrue()
+        {
+            return true;
+        }
+
+        public static object Passthrough(object obj)
+        {
+            return obj;
+        }
+    }
+}
